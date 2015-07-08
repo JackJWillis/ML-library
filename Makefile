e8ff632c5bf34a1deb4e672a16b8c36548acce2f
Jonathan Glidden <jhglidden@gmail.com>
2015-07-08 13:00:11 -0700

    fix paths

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ all: tanzania
 tanzania: analyses/tanzania.html
 
 analyses/tanzania.html: data/tanzania_cv_out.csv data/tanzania.csv
-	cd analyses; Rscript report.R tanzania; cd ..
+	Rscript analyses/report.R tanzania
 
 data/tanzania_cv_out.csv: analyses/tanzania.R R/model.R
 	Rscript analyses/tanzania.R
--- analyses/report.R
+++ analyses/report.R
@@ -3,7 +3,8 @@ library(MLlibrary)
 
 args <- commandArgs(TRUE)
 NAME <- args[1]
-knit2html('report.Rmd', output=NAME)
-joined <- load <- models(NAME)
-reach_df <- calculate_reach_(NAME)
+dataset <- load_dataset(NAME)
+joined <- load_models(NAME)
+knit2html('analyses/report.Rmd', output=NAME)
+reach_df <- calculate_reach_(joined, base='least_squares')
 write.csv(reach_df, paste(NAME, '.csv', sep=''))
