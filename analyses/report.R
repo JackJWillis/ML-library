d603fa239592138859461b666408cf02e1c66d9f
Jonathan Glidden <jhglidden@gmail.com>
2015-07-07 18:47:49 -0700

    write reach table

--- analyses/report.R
+++ analyses/report.R
@@ -1,5 +1,9 @@
 library(knitr)
+library(MLlibrary)
 
 args <- commandArgs(TRUE)
 NAME <- args[1]
 knit2html('report.Rmd', output=NAME)
+joined <- load <- models(NAME)
+reach_df <- calculate_reach_(NAME)
+write.csv(reach_df, paste(NAME, '.csv', sep=''))
