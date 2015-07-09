eedbcbbe2d1f826efe6b3f0db5792a6717a8b794
Jonathan Glidden <jhglidden@gmail.com>
2015-07-09 10:30:00 -0700

    show nas in joined results

--- analyses/join.R
+++ analyses/join.R
@@ -3,6 +3,6 @@ paths <- commandArgs(TRUE)
 df <- read.csv(paths[1])
 for (path in paths[-1]) {
   new_df <- read.csv(path)
-  df <- merge(df,  new_df, by='method')
+  df <- merge(df,  new_df, by='method', all=TRUE)
 }
 write.csv(df, 'results/scores.csv', row.names=FALSE)
