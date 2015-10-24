6867d43188e2a709f58e7500438d7c75e1a760e0
Jonathan Glidden <jhglidden@gmail.com>
2015-10-24 11:53:55 -0700

    turn south africa into nice script

--- analyses/south_africa.R
+++ analyses/south_africa.R
@@ -1,47 +1,65 @@
 library(dplyr)
 library(foreign)
 
-base_name <- 'W1_Anon_V5.3.dta'
-get_data <- function(prefix) {
+EXCLUDE_ADMIN <- c(
+  'h_pcode',
+  'h_pcode_pid',
+  'h_phase',
+  'h_preflng',
+  'h_intvr_c',
+  'h_intvr_imp',
+  'h_intrv_d',
+  'h_intrv_m',
+  'h_intrv_y',
+  'h_intrvsrt')
+EXCLUDE_FOOD_CONS <- 'h_fd'
+EXCLUDE_NFOOD_CONS <-  'h_nf'
+
+get_data <- function(prefix, base_name) {
   fname <- paste(prefix, base_name, sep='_')
   fpath <- paste(TARGETING_DATA_IN, 'south_africa', fname, sep='/')
   read.dta(fpath)
 }
 
-admin <- get_data('Admin')
-adult <- get_data('Adult')
-child <- get_data('Child')
-hhderived <- get_data('hhderived')
-hhquest <- get_data('HHQuestionnaire')
-household_roster <- get_data('HouseholdRoster')
-indderived <- get_data('indderived')
-proxy <- get_data('Proxy')
-
-expenditures <- select(hhderived, w1_hhid, w1_expenditure)
-
-exclude_quest <- c(
-  'w1_h_pcode',
-  'w1_h_pcode_pid',
-  'w1_h_phase',
-  'w1_h_preflng',
-  'w1_h_intvr_c',
-  'w1_h_intvr_imp',
-  'w1_h_intrv_d',
-  'w1_h_intrv_m',
-  'w1_h_intrv_y',
-  'w1_h_intrvsrt')
-hhquest <- select(hhquest, -one_of(exclude_quest))
-df <- merge(expenditures, hhquest, by='w1_hhid')
-df <- rename(df, yyyyy=w1_expenditure)
-
-quick_test <- function(survey_df, method=ols) {
-  survey_df <- discard(survey_df, ~sum(is.na(.x)) / nrow(survey_df) > .7)
-  survey_df <- na_indicator(survey_df)
-  holdout_fraction <- 0.1
-  assignments <- as.logical(rbinom(nrow(survey_df), 1, 1 - holdout_fraction))
-  train <- survey_df[assignments, ]
-  test <- survey_df[!assignments, ]
-  train <- keep(train, ~length(unique(.x)) > 1)
-  fold <- list(train=train, test=test)
-  method(fold)
+p_wave_column <- function(wave_number, colname) {
+  paste('w', wave_number, '_', colname, sep='')
 }
+
+create_dataset <- function(wave_number) {
+  pwave <- function(colname) p_wave_column(wave_number, colname)
+  base_name <- paste('W', wave_number, '_Anon_V5.3.dta', sep='')
+  hhderived <- get_data('hhderived')
+  hhquest <- get_data('HHQuestionnaire')
+  
+  hhid <- pwave('hhid')
+  df <- merge(expenditures, hhquest, by=pwave)
+  
+  cons_colnames <- pwave('expenditure')
+  df <- rename_(df, .dots=setNAMES(cons_colnames, TARGET_VARIABLE))
+  df <- select(df, -one_of(hhid))
+  
+  exclude_admin <- sapply(EXCLUDE_ADMIN, pwave)
+  df <- select(df, -one_of(exclude_admin))
+  
+  exclude_food_cons <- pwave(EXCLUDE_FOOD_CONS)
+  exclude_nfood_cons <- pwave(EXCLUDE_FOOD_CONS)
+  df <- select(df, -contains(exclude_food_cons))
+  df <- select(df, -contains(exclude_nfood_cons))
+  df
+}
+
+run_wave <- function(wave_number) {
+  wave_name <- paste('south_africa_', 'w', wave_number, sep='')
+  df <- create_dataset(wave_number)
+  df <- na_indicator(df)
+  df <- standardize_predictors(df, TARGET_VARIABLE)
+  output <- test_all(df)
+  save_validation_models_(year_name, output)
+}
+
+print(1)
+run_wave(1)
+print(2)
+run_wave(2)
+print(3)
+run_wave(3)
